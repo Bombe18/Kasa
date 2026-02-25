ff6db8e385c6b75c793f725ea18262f8f051c293
Colin Cyril <cyrilco18@gmail.com>
2026-02-25 11:50:30 +0100

    Added font monserrat + gallery

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Footer from "./components/footer"
 import Header from "./components/header"
+import Gallery from "./components/Gallery.jsx"
+import "./index.scss"
+<link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'></link>
+
 
 const App = () => {
   return (
     <>
       <Header />
+      <Gallery />
       <Footer />
     </>
   )
