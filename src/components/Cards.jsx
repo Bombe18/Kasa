4e8c70930219f67d48f4d2e94ce15ca859977552
Colin Cyril <cyrilco18@gmail.com>
2026-02-27 15:21:57 +0100

    card info + css

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -14,22 +14,34 @@ const Cards = () => {
             {logements.map((logement) => (
                 <div className="card" key={logement.id}>
                     <img className="card__image" src={logement.cover} alt={logement.title} />
-                    <h2 className="card__title">{logement.title}</h2>
-                    <div className="card__location">{logement.location}</div>
-                    <div className="card__host">
-                        <div className="card__host_name">{logement.host.name}</div>
-                        <div className="card__rating"><Rating scaleValue={logement.rating} /></div>
-                        <div className="card__tags">
+                    <div className="appartment__info">
+                        <div className="appartment__location">
+                            <h2 className="card__title">{logement.title}</h2>
+                            <h3 className="card__location">{logement.location}</h3>
+                        </div>
+
+                        <div>
+                            <div className="card__host">
+                                <div className="card__host_name">{logement.host.name}</div>
+                                <div ><img className="host__picture" src={logement.host.picture} alt={"photo de " + logement.host.name} /></div>
+                            </div></div>
+                    </div>
+
+                    <div >
+                        <div className="card__Tags_And_Rating">
                             <div className="card__tags-list">{scaleTag(logement.tags)}</div>
+
+                            <div className="card__rating"><Rating scaleValue={logement.rating} /></div>
                         </div>
                     </div>
-                    <div className="card_info">
+
+                    <div className="card__info">
                         <div className="card__description">{logement.description}</div>
                         <div className="card__equipement">
                             <ul className="card__equipments_list">
                                 {logement.equipments.map((equipment, index) => (
                                     <li key={index}>{equipment}</li>
-                                    ))}
+                                ))}
                             </ul>
                         </div>
                     </div>
