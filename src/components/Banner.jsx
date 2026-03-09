d1a0c44b02769c1c3f455c41b91e80b383d8321c
Colin Cyril <cyrilco18@gmail.com>
2026-03-09 16:17:10 +0100

    adding title as param. Should be personnalisable

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,13 +1,11 @@
 import "../styles/_banner.scss"
-import background from '../assets/background.webp'
 
 
-export default function Banner() {
-    const backgroundImage = background;
+export default function Banner({image, alt, title}) {
     return (
         <div className="gallery_banner_background">
-            <img className='img__banner_background' src={backgroundImage} alt="Paysage de côte avec des falaises et des vagues" />
-            <h1 className="title">Chez vous, partout et ailleurs</h1>
+            <img className='img__banner_background' src={image} alt={alt} />
+            <h1 className="title">{title}</h1>
         </div>
     )
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,14 @@
 import '../styles/_container1240.scss'
 import Logements from "./Logements"
 import Banner from './Banner.jsx'
+import background from '../assets/background.webp' 
 
 const Gallery = () => {
+    const backgroundImage = background;
+
     return (
         <section className="container-1240">
-            <Banner />
+            <Banner image={backgroundImage} alt="Paysage de côte avec des falaises et des vagues" title="Chez vous, partout et ailleurs"/>
 
             <div className="gallery_background">
                 <div className="gallery_content">
