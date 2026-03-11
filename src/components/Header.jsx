34c0efe855ba99d5752eb0afc791926a86d1b5cf
Colin Cyril <cyrilco18@gmail.com>
2026-03-11 15:07:40 +0100

    Adding nav link when on the page.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import '../styles/_header.scss'
 import '../styles/_container1240.scss'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import logo from '../assets/LogoMain.png'
 
 const Header = () => {
+   
     return (
         <header className="header container-1240">
             <div className="header__container">
@@ -12,11 +13,26 @@ const Header = () => {
                     alt="Kasa logo"
                     className="nav-logo" />
                 <nav className="nav_container">
-                    <Link to="/">Accueil</Link>
-                    <Link to="/about">À propos</Link>
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                            isActive ? 'nav-link active' : 'nav-link'
+                        }
+                    >
+                        Accueil
+                    </NavLink>
+                    <NavLink
+                        to="/about"
+                        className={({ isActive }) =>
+                            isActive ? 'nav-link active' : 'nav-link'
+                        }
+                    >
+                        À propos
+                    </NavLink>
                 </nav>
             </div>
-        </header>)
+        </header>
+    )
 }
 
 export default Header
