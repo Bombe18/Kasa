231ce530a76ab64e8e923a169e7ed74e030e96bc
Colin Cyril <cyrilco18@gmail.com>
2026-03-20 12:49:15 +0100

    line break

--- src/Scripts/utils.js
+++ src/Scripts/utils.js
@@ -2,8 +2,10 @@ export const getCustomClass = (variant) => {
     switch (variant) {
         case 'product-dropdown':
             return 'product--dropdown';
+
         case 'about-dropdown':
             return 'about--dropdown';
+            
         case 'about-background':
             return 'about--background';
 
