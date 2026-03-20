55be07f73639b8c0ec5dee0edd90c198d197ad61
Colin Cyril <cyrilco18@gmail.com>
2026-03-20 12:48:31 +0100

    deleting const img

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -3,19 +3,17 @@ import activeStar from "../assets/star-active.png"
 import inactiveStar from  "../assets/star-inactive.png"
 
 const Rating = ({ scaleValue }) => {
-    const range = [1, 2, 3, 4, 5]
-const activedStar = activeStar
-const inactivedStar = inactiveStar    
+    const range = [1, 2, 3, 4, 5]  
     return (
         <div className="rating">
             {range.map((index) =>
                 scaleValue >= index ? (
                     <span key={index} >
-                        <img className="rating__star rating__star_active" src={activedStar} alt="Étoile active" />
+                        <img className="rating__star rating__star_active" src={activeStar} alt="Étoile active" />
                     </span>
                 ) : (
                     <span key={index}>
-                        <img className="rating__star" src={inactivedStar} alt="Étoile inactive" />
+                        <img className="rating__star" src={inactiveStar} alt="Étoile inactive" />
                     </span>
                 )
             )}
