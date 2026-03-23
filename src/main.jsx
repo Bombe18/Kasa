8d69d92a6b9f492a13e9e1b9b0e7bab6bf9d05cd
Colin Cyril <cyrilco18@gmail.com>
2026-03-23 13:11:43 +0100

    changing declarative mode router to data mode router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react"
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
 import AppRouter from './router.jsx'
 import "./index.scss"
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppRouter />
+    <RouterProvider router={AppRouter} />
   </React.StrictMode>
 )
--- src/router.jsx
+++ src/router.jsx
@@ -1,31 +1,64 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import HomePage from "./pages/homepage/Home.jsx";
+import Home from "./pages/homepage/Home.jsx";
 import Footer from "./components/Footer.jsx";
 import Header from "./components/Header.jsx";
 import About from "./pages/about/About.jsx";
 import Product from "./pages/product/Product.jsx";
 import NotFound from "./pages/notFound/NotFound.jsx";
-import Preload from "./components/Preload.jsx";
 import "./index.scss";
-
-const AppRouter = () => (
-  <Router>
-    <Header />
-    <Preload />
-
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/product/:id" element={<Product />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-    <Footer />
-  </Router>
-);
-
-export default AppRouter;
+import { getLogements } from "./scripts/logementsDatas.js";
+import { createBrowserRouter, Outlet } from "react-router-dom";
 
 
 
- 
+function RootLayout() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
+
+const AppRoutes = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        loader: async() => {
+          return await getLogements();
+        },
+        element: <Home />,
+      },
+      {
+        path: "product/:id",
+
+        element: <Product />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      }],
+  }]);
+
+
+
+export default AppRoutes;
+
+
+
+//<Route path="/" element={<HomePage />} />
+//<Route path="/about" element={<About />} />
+//<Route path="/product/:id" element={<Product />} />
+//<Route path="*" element={<NotFound />} />
+
+
+
+
+
